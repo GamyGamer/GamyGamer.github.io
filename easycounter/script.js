4a4d0efdd153c3511c6b8991d120b9a6f67814be
GamyGamer <49346764+GamyGamer@users.noreply.github.com>
2022-03-22 20:41:21 +0100

    Added first page for score counter and mainpage (more todo)

---
+++ easycounter/script.js
@@ -0,0 +1,17 @@
+function druzyna1(input){
+    if (input == 0) {
+        window.document.getElementById("dr1").innerHTML = 0 
+        return 0;
+    }
+    window.document.getElementById("dr1").innerHTML = parseInt(window.document.getElementById("dr1").innerHTML) +input 
+
+
+
+}
+function druzyna2(input){
+    if (input == 0) {
+        window.document.getElementById("dr2").innerHTML = 0 
+        return 0;
+    }
+    window.document.getElementById("dr2").innerHTML = parseInt(window.document.getElementById("dr2").innerHTML) +input 
+}
